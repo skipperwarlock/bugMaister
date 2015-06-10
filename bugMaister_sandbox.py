1039f6055b8cbbb59b91ab4c8775a41b61430634
Sam Cantrell <scantrel@redhat.com>
2015-06-10 17:53:59 -0400

    added progress counter

--- bugMaister_sandbox.py
+++ bugMaister_sandbox.py
@@ -31,6 +31,10 @@ def loadTickets():
             result = json.load(result_file)
         return result
 
+def printLoadBar(ticketCt, ticketTotal):
+        print str(int((ticketCt/ticketTotal) * 100)) + "% Complete   \r",
+        return
+
 percentFilter       = 50
 descSearchString    = "Description:"
 descSearchStringLen = len(descSearchString)
@@ -55,53 +59,61 @@ iDescTags           = filterTags(iDescTags)
 #loop through bugs saved locally and
 #compare keywords with input bug
 ticketHistory = loadTickets()
+ticketCt      = 0.0
+ticketTotal   = len(ticketHistory["result"])
 for ticket in ticketHistory["result"]:
+        ticketCt           += 1
         subjMatchValue      = 0
         descMatchValue      = 0
         bugDesc             = str(ticket["description"])
         bugSubj             = str(ticket["short_description"])
         bugID               = str(ticket["number"])
-        bugIds              = bugIds + [bugID]
-        bugSubjTokens       = word_tokenize(bugSubj)
-        bugSubjTags         = pos_tag(bugSubjTokens)
-        bugSubjTags         = filterTags(bugSubjTags)
-        bugDescTokens       = word_tokenize(bugDesc)
-        bugDescTags         = pos_tag(bugDescTokens)
-        bugDescTags         = filterTags(bugSubjTags)
+        bugState            = int(ticket["state"])
+        printLoadBar(ticketCt, ticketTotal)
+        if bugState == 7 or bugState == 6:
+                bugIds              = bugIds + [bugID]
+                bugSubjTokens       = word_tokenize(bugSubj)
+                bugSubjTags         = pos_tag(bugSubjTokens)
+                bugSubjTags         = filterTags(bugSubjTags)
+                bugDescTokens       = word_tokenize(bugDesc)
+                bugDescTags         = pos_tag(bugDescTokens)
+                bugDescTags         = filterTags(bugSubjTags)
 
-        #subject to subject
-        for bugWord in bugSubjTags:
-                for inputWord in iSubjTags:
-                        if str(bugWord).upper()==str(inputWord).upper():
-                            subjMatchValue += 1
-                            break
-        #subject to description
-        for bugWord in bugSubjTags:
-                for inputWord in iDescTags:
-                        if str(bugWord).upper()==str(inputWord).upper():
-                            subjMatchValue += 1
+                #subject to subject
+                for bugWord in bugSubjTags:
+                        for inputWord in iSubjTags:
+                                if str(bugWord).upper()==str(inputWord).upper():
+                                    subjMatchValue += 1
+                                    break
+                #subject to description
+                #note that the missing break is intentional
+                #it increases the priority of these bugs
+                for bugWord in bugSubjTags:
+                        for inputWord in iDescTags:
+                                if str(bugWord).upper()==str(inputWord).upper():
+                                    subjMatchValue += 1
 
-        #description to description
-        for bugWord in bugDescTags:
-                for inputWord in iDescTags:
-                        if str(bugWord).upper()==str(inputWord).upper():
-                            descMatchValue += 1
-                            break
-        #description to subject
-        for bugWord in bugDescTags:
-                for inputWord in iSubjTags:
-                        if str(bugWord).upper()==str(inputWord).upper():
-                            descMatchValue += 1
-                            break
+                #description to description
+                for bugWord in bugDescTags:
+                        for inputWord in iDescTags:
+                                if str(bugWord).upper()==str(inputWord).upper():
+                                    descMatchValue += 1
+                                    break
+                #description to subject
+                for bugWord in bugDescTags:
+                        for inputWord in iSubjTags:
+                                if str(bugWord).upper()==str(inputWord).upper():
+                                    descMatchValue += 1
+                                    break
 
-        if len(bugSubjTags) != 0:
-            subjMatchPercentage = (float(subjMatchValue) / len(bugSubjTags)) * 100.0
-            descMatchPercentage = (float(descMatchValue) / len(bugDescTags)) * 100.0
-        else:
-            subjMatchPercentage = 0.0
-            descMatchPercentage = 0.0
+                if len(bugSubjTags) != 0:
+                    subjMatchPercentage = (float(subjMatchValue) / len(bugSubjTags)) * 100.0
+                    descMatchPercentage = (float(descMatchValue) / len(bugDescTags)) * 100.0
+                else:
+                    subjMatchPercentage = 0.0
+                    descMatchPercentage = 0.0
 
-        bugMatchValues = bugMatchValues + [(subjMatchPercentage+descMatchPercentage) / 2]
+                bugMatchValues = bugMatchValues + [(subjMatchPercentage+descMatchPercentage) / 2]
 
 #loop through bugs and output match percentage
 ct = 0
